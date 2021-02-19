7a5c1a892899ab90e9ae63855f585d2d5c08ab86
EStolyarova <elena.stolyarova@le-campus-numerique.fr>
2021-02-19 14:58:35 +0100

    carte de france

--- carte.py
+++ carte.py
@@ -1,3 +1,22 @@
 import geopandas as gpd
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("donnees-hospitalieres-covid19-2021-02-18-19h03.csv", delimiter=';')
+print(df.head())
+
 sf = gpd.read_file('departements-version-simplifiee.geojson')
-print(sf.head())
+
+
+dep_to_check = set(sf.code.unique())
+print(dep_to_check)
+print(sf)
+df = df.loc[df.dep.isin(dep_to_check) == True]
+
+
+fig = px.choropleth_mapbox(df, geojson=sf, color="dc",
+                           locations="dep", featureidkey="properties.code",
+                           center={"lat": 46.22, "lon": 2.21},
+                           mapbox_style="carto-positron", zoom=3, range_color=[0, 500])
+
+fig.show()
